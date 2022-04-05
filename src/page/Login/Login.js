6ee325a8222f052767381f0c88611cb9f5aae0c6
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-05 20:59:48 +0200

    update

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../../features/userSlice'
+import { auth } from '../../Firebase'
 import './Login.css'
 function Login() {
-  const register = () => {}
-  const loginSubmit = () => {}
+  const [person, setPerson] = useState({
+    name: '',
+    photoUrl: '',
+    email: '',
+    password: '',
+  })
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({ ...person, [name]: value })
+  }
+  const dispatch = useDispatch()
+  const register = () => {
+    if (person.name && person.email && person.password) {
+      auth
+        .createUserWithEmailAndPassword(person.email, person.password)
+        .then((userAuth) => {
+          userAuth.user
+            .updateProfile({
+              displayName: person.name,
+              photoURL: person.photoUrl,
+            })
+            .then(() => {
+              dispatch(
+                login({
+                  email: userAuth.user.email,
+                  uid: userAuth.user.displayName,
+                  displayName: person.name,
+                  photoURL: person.photoUrl,
+                })
+              )
+            })
+        })
+        .catch((err) => console.log(err.message))
+    } else {
+      console.log('false')
+    }
+  }
+  const loginSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className='login'>
       <img
@@ -10,18 +52,42 @@ function Login() {
         alt='LinkedIn_logo'
       />
       <form action=''>
-        <input type='text' placeholder='Full name (required if registering)' />
-        <input type='text' placeholder='Profile pic Url (optional)' />
-        <input type='email' placeholder='Email (required if registering)' />
-        <input type='password' placeholder='Password' />
+        <input
+          type='text'
+          value={person.name}
+          name='name'
+          onChange={handleChange}
+          placeholder='Full name (required if registering)'
+        />
+        <input
+          type='text'
+          value={person.photoUrl}
+          name='photoUrl'
+          onChange={handleChange}
+          placeholder='Profile pic Url (optional)'
+        />
+        <input
+          type='email'
+          value={person.email}
+          name='email'
+          onChange={handleChange}
+          placeholder='Email (required if registering)'
+        />
+        <input
+          type='password'
+          value={person.password}
+          name='password'
+          onChange={handleChange}
+          placeholder='Password'
+        />
         <button type='submit' onClick={loginSubmit}>
           Sign In
         </button>
         <p>
-          Not a member?{' '}
+          Not a member ?{' '}
           <span className='login_register' onClick={register}>
             Register Now
-          </span>{' '}
+          </span>
         </p>
       </form>
     </div>
