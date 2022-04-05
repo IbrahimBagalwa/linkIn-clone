815f013b2d82d5dff616d9baff5f6aed18b46dd6
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-05 16:45:56 +0200

    post building

--- src/components/Feed/Posts.js
+++ src/components/Feed/Posts.js
@@ -1,6 +1,10 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
-
+import InputOptions from './InputOptions'
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined'
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined'
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined'
 function Posts({ name, description, message, photoUrl }) {
   return (
     <div className='posts'>
@@ -13,6 +17,12 @@ function Posts({ name, description, message, photoUrl }) {
       </div>
       <div className='post_body'>
         <p>{message}</p>
+      </div>
+      <div className='post_buttons'>
+        <InputOptions Icon={ThumbUpAltOutlinedIcon} title='Like' color='gray' />
+        <InputOptions Icon={ChatOutlinedIcon} title='Comment' color='gray' />
+        <InputOptions Icon={ShareOutlinedIcon} title='Share' color='gray' />
+        <InputOptions Icon={SendOutlinedIcon} title='Send' color='gray' />
       </div>
     </div>
   )
