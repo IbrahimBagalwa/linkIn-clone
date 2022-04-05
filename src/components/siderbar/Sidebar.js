628519c872ea211b8675c3ac99d34dea4297e340
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-05 15:11:55 +0200

    sidebar left done

--- src/components/siderbar/Sidebar.js
+++ src/components/siderbar/Sidebar.js
@@ -2,18 +2,30 @@ import { Avatar } from '@mui/material'
 import React from 'react'
 import './Sidebar.css'
 function Sidebar() {
+  const recentItem = (topic) => {
+    return (
+      <div className='sidebar_recent__item'>
+        <span className='sidebar_hash'>#</span>
+        <p>{topic}</p>
+      </div>
+    )
+  }
+
   return (
     <div className='sidebar'>
       <div className='sidebar_top'>
-        <img src='' alt='' />
+        <img
+          src='https://images.pexels.com/photos/133633/pexels-photo-133633.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+          alt='cover_img'
+        />
         <Avatar className='sidebar_avatar' />
-        <h1>Ibrahim Bagalwa</h1>
+        <h2>Ibrahim Bagalwa</h2>
         <h4>software Engineer</h4>
       </div>
 
       <div className='sidebar_stats'>
         <div className='sidebar_stat'>
-          <p>Who viewed your prifile</p>
+          <p>Who viewed you </p>
           <p className='sidebar_stat__number'>62</p>
         </div>
         <div className='sidebar_stat'>
@@ -22,8 +34,13 @@ function Sidebar() {
         </div>
       </div>
 
-      <div className='sidebar_bottons'>
+      <div className='sidebar_botton'>
         <p>Recent</p>
+        {recentItem('Reactjs')}
+        {recentItem('Programming')}
+        {recentItem('Software Developer')}
+        {recentItem('Design')}
+        {recentItem('Developer')}
       </div>
     </div>
   )
