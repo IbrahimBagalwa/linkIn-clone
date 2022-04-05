3d1468c1c462f86f71bceaf639be49eca12820bf
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-05 19:33:07 +0200

    update login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './App.css'
 import Feed from './components/Feed/Feed'
 import Header from './components/Header/Header'
 import Sidebar from './components/siderbar/Sidebar'
+import { selectUser } from './features/userSlice'
+import Login from './page/Login/Login'
 
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className='app'>
       <Header />
-      <div className='app_body'>
-        <Sidebar />
-        <Feed />
-        {/* widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app_body'>
+          <Sidebar />
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
     </div>
   )
 }
--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import './Login.css'
 function Login() {
-  return <div>Login</div>
+  return (
+    <div className='login'>
+      <img
+        src='https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks'
+        alt='LinkedIn_logo'
+      />
+    </div>
+  )
 }
 
 export default Login
