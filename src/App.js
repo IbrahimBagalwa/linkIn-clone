52ba0a26edb2e8ab18f38d28b7c4c1e1e690a9f6
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-08 10:19:47 +0200

    implemente on auth state changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import Feed from './components/Feed/Feed'
 import Header from './components/Header/Header'
 import Sidebar from './components/siderbar/Sidebar'
-import { selectUser } from './features/userSlice'
+import { login, logout, selectUser } from './features/userSlice'
+import { auth } from './Firebase'
 import Login from './page/Login/Login'
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // user iis logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        )
+      } else {
+        // user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
+
   return (
     <div className='app'>
       <Header />
